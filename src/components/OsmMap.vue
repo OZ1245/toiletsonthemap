8a513da6510b2730fdac690eded9635963d0b0fa
Boldyreva Nadezhda <boldyrevanadezhda@gmail.com>
2022-11-02 11:20:51 +0700

    GPS. Geolocation request

--- src/components/OsmMap.vue
+++ src/components/OsmMap.vue
@@ -5,6 +5,8 @@
       v-model:zoom="zoom"
       :center="center"
       class="map"
+
+      @move="log('move')"
     >
       <l-tile-layer
         :url="url"
@@ -24,23 +26,19 @@ import "leaflet/dist/leaflet.css";
 const url = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 const attribution = '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors';
 
-const zoom = ref(2);
-const center = ref([47.41322, -1.219482]);
+const zoom = 10;
+const center = [47.41322, -1.219482];
 
 onMounted(() => {
   if (navigator.geolocation) {
     // Geolocation available
-    const geolocation = navigator.geolocation
+    const geolocation = navigator.geolocation;
 
-    geolocation.watchPosition(result => {
+    geolocation.watchPosition((result) => {
       console.log(result)
-    })
-
-    geolocation.getCurrentPosition((result) => {
-      console.log(result)
-      center.value = [result.coords.latitude, result.coords.longitude];
-      zoom.value = 10;
+      center = [result.coords.latitude, result.coords.longitude];
     }, (result) => {
+      center = [47.41322, -1.219482];
       throw result;
     });
   }
